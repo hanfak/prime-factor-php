3437e5765c143222dcf3da1494b50ef4d30880dd
Hanif Fakira <fakira.work@gmail.com>
2016-12-13 09:18:19 +0000

    T1RF extracted test and init of obj to setup

--- primeFactor/Test/PrimeFactorTest.php
+++ primeFactor/Test/PrimeFactorTest.php
@@ -4,14 +4,16 @@ namespace primeFactor\Test;
 use primeFactor\src\PrimeFactor;
 
 class PrimeFactorTest extends \PHPUnit_Framework_TestCase {
-    public function testPrimeFactorObject() {
-      $primeFactor = new PrimeFactor();
+
+    public function setUp() {
+      parent::setUp();
+      $this->primeFactor = new PrimeFactor();
     }
 
     public function test1DecompositionOf2(){
-      $primeFactor = new PrimeFactor();
       $expected = [2];
-      $actual = $primeFactor->decompose(2);
+      $actual = $this->primeFactor->decompose(2);
+      
       $this->assertEquals($expected, $actual);
     }
 
