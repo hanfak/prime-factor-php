225c8917b9b177d0422d54178fd8ecf78e92acae
Hanif Fakira <fakira.work@gmail.com>
2016-12-13 11:21:52 +0000

    RF outer while to for loop

--- primeFactor/src/PrimeFactor.php
+++ primeFactor/src/PrimeFactor.php
@@ -6,13 +6,11 @@ class PrimeFactor
 {
     public function decompose($number){
         $factors = [];
-        $factor = 2;
 
-        while($number > 1) {
+        for($factor = 2; $number > 1; $factor += 1) {
           for(; $number % $factor === 0; $number /= $factor){
             array_push($factors, $factor);
           }
-          $factor += 1;
         }
 
         return $factors;
